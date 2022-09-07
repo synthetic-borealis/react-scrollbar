40708a9d4b7f40d9ebdbc9d888d196ad449a398e
synthetic-borealis <snip.coloured@gmail.com>
2022-09-07 22:15:51 +0300

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import './App.scss';
+
+import ScrollBarArea from './ScrollBarArea';
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="app__title">
+        ScrollBar Area Component
+      </h1>
+      <ScrollBarArea>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Mattis rhoncus urna neque viverra justo. Vestibulum
+          lectus mauris ultrices eros. Ipsum a arcu cursus vitae congue. Est placerat in egestas
+          erat imperdiet sed. Malesuada bibendum arcu vitae elementum curabitur vitae. Elit eget
+          gravida cum sociis. Aliquet eget sit amet tellus cras. Cras pulvinar mattis nunc sed
+          blandit libero volutpat sed. Felis eget nunc lobortis mattis aliquam. Et malesuada fames
+          ac turpis egestas sed tempus urna. Auctor neque vitae tempus quam pellentesque. Id
+          interdum velit laoreet id donec. Aliquet enim tortor at auctor urna nunc.
+        </p>
+        <p>
+          Faucibus et molestie ac feugiat. Sed nisi lacus sed viverra tellus. Sed augue lacus
+          viverra vitae. Egestas sed sed risus pretium quam vulputate dignissim. Eget velit aliquet
+          sagittis id consectetur purus ut faucibus. Tristique senectus et netus et malesuada fames
+          ac turpis egestas. Lobortis feugiat vivamus at augue eget arcu dictum varius. Venenatis
+          urna cursus eget nunc scelerisque viverra mauris in aliquam. Ac placerat vestibulum lectus
+          mauris ultrices eros. Aliquet enim tortor at auctor urna. Gravida quis blandit turpis
+          cursus in hac habitasse platea dictumst. Velit laoreet id donec ultrices tincidunt arcu
+          non sodales neque. Commodo elit at imperdiet dui accumsan.
+        </p>
+        <p>
+          Molestie at elementum eu facilisis sed odio morbi. Tortor condimentum lacinia quis vel
+          eros. Eget egestas purus viverra accumsan. Viverra vitae congue eu consequat ac felis
+          donec. Pulvinar neque laoreet suspendisse interdum consectetur libero. Aliquam sem
+          fringilla ut morbi tincidunt. Metus vulputate eu scelerisque felis imperdiet proin. Leo
+          vel fringilla est ullamcorper eget nulla facilisi etiam. Malesuada nunc vel risus commodo
+          viverra maecenas. Semper auctor neque vitae tempus quam pellentesque nec nam aliquam.
+        </p>
+        <p>
+          Scelerisque varius morbi enim nunc faucibus a pellentesque sit amet. Aliquam ut porttitor
+          leo a diam sollicitudin. Sit amet nisl purus in mollis nunc sed id. Pharetra vel turpis
+          nunc eget lorem dolor sed viverra ipsum. Tincidunt vitae semper quis lectus nulla at
+          volutpat. Habitant morbi tristique senectus et netus. Feugiat in fermentum posuere urna.
+          Aliquet porttitor lacus luctus accumsan tortor posuere ac. Duis ultricies lacus sed turpis
+          tincidunt id. Id nibh tortor id aliquet lectus proin.
+        </p>
+        <p>
+          Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Nibh ipsum
+          consequat nisl vel pretium. Adipiscing tristique risus nec feugiat in fermentum.
+          Ullamcorper malesuada proin libero nunc consequat interdum varius sit. Magna etiam tempor
+          orci eu lobortis elementum nibh tellus. Cursus turpis massa tincidunt dui ut ornare.
+          Tincidunt vitae semper quis lectus nulla at volutpat diam ut. Et netus et malesuada fames
+          ac turpis egestas sed tempus. Velit aliquet sagittis id consectetur. Dolor morbi non arcu
+          risus quis varius quam quisque id. Nec tincidunt praesent semper feugiat nibh. Odio tempor
+          orci dapibus ultrices in. Tincidunt vitae semper quis lectus nulla at. Tristique senectus
+          et netus et malesuada fames ac turpis egestas. Tortor at risus viverra adipiscing. Vitae
+          semper quis lectus nulla at. Feugiat in ante metus dictum at tempor commodo ullamcorper a.
+          Nisi lacus sed viverra tellus in hac. Volutpat est velit egestas dui id ornare arcu odio
+          ut. Et malesuada fames ac turpis egestas.
+        </p>
+      </ScrollBarArea>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ScrollBarArea.tsx
@@ -0,0 +1,185 @@
+import React, { ReactNode, useCallback, useEffect, useRef, useState, } from 'react';
+import PropTypes from 'prop-types';
+import './ScrollBarArea.scss';
+
+interface IScrollBarAreaProps {
+  children?: ReactNode;
+}
+
+function ScrollBarArea({ children }: IScrollBarAreaProps) {
+  const contentRef = useRef<HTMLDivElement>(null);
+  const scrollTrackRef = useRef<HTMLDivElement>(null);
+  const scrollThumbRef = useRef<HTMLDivElement>(null);
+  const observer = useRef<ResizeObserver | null>(null);
+  const [thumbHeight, setThumbHeight] = useState<number>(20);
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const [scrollStartPosition, setScrollStartPosition] = useState<number>(0);
+  const [initialScrollTop, setInitialScrollTop] = useState<number>(0);
+
+  function handleResize(ref: HTMLDivElement, trackSize: number) {
+    const { clientHeight, scrollHeight } = ref;
+    setThumbHeight(Math.max((clientHeight / scrollHeight) * trackSize, 20));
+  }
+
+  function handleWheel(evt: React.WheelEvent) {
+    evt.stopPropagation();
+    if (contentRef.current) {
+      contentRef.current.scrollBy({
+        top: evt.deltaY,
+        behavior: 'smooth',
+      });
+    }
+  }
+
+  const handleThumbPosition = useCallback(
+    () => {
+      if (
+        !contentRef.current ||
+        !scrollTrackRef.current ||
+        !scrollThumbRef.current
+      ) {
+        return;
+      }
+      const {
+        scrollTop: contentTop,
+        scrollHeight: contentHeight,
+      } = contentRef.current;
+      const {
+        clientHeight: trackHeight,
+      } = scrollTrackRef.current;
+      const newTop = Math.min(
+        (contentTop / contentHeight) * trackHeight,
+        trackHeight - thumbHeight,
+      );
+      const thumb = scrollThumbRef.current;
+
+      thumb.style.top = `${newTop}px`;
+    },
+    [thumbHeight]
+  );
+
+  const handleTrackClick = useCallback(
+    (evt: React.PointerEvent<HTMLDivElement>) => {
+      evt.preventDefault();
+      evt.stopPropagation();
+      const [
+        { current: trackCurrent },
+        { current: contentCurrent }
+      ] = [scrollTrackRef, contentRef];
+
+      if (trackCurrent && contentCurrent) {
+        const { clientY } = evt;
+        const target = evt.target as HTMLDivElement;
+        const rect = target.getBoundingClientRect();
+        const trackTop = rect.top;
+        const thumbOffset = -(thumbHeight / 2);
+        const clickRatio = (clientY - trackTop + thumbOffset) / trackCurrent.clientHeight;
+        const scrollAmount = Math.floor(clickRatio * contentCurrent.scrollHeight);
+        contentCurrent.scrollTo({
+          top: scrollAmount,
+          behavior: 'smooth',
+        });
+      }
+    },
+    [thumbHeight]
+  );
+
+  const handleThumbPointerDown = useCallback(
+    (evt: React.PointerEvent<HTMLDivElement>) => {
+      if (!(evt.pointerType === 'mouse' && evt.button !== 0)) {
+        evt.preventDefault();
+        evt.stopPropagation();
+        setScrollStartPosition(evt.clientY);
+        if (contentRef.current) {
+          setInitialScrollTop(contentRef.current.scrollTop);
+        }
+        setIsDragging(true);
+      }
+    },
+    []
+  );
+
+  const handleThumbPointerUp = useCallback(
+    (evt: React.PointerEvent<HTMLDivElement>) => {
+      if (!(evt.pointerType === 'mouse' && evt.button !== 0) || evt.type === 'pointerleave') {
+        evt.preventDefault();
+        evt.stopPropagation();
+        if (isDragging) {
+          setIsDragging(false);
+        }
+      }
+    },
+    [isDragging]
+  );
+
+  const handleThumbPointerMove = useCallback(
+    (evt: React.PointerEvent<HTMLDivElement>) => {
+      evt.preventDefault();
+      evt.stopPropagation();
+      if (isDragging && contentRef.current) {
+        const {
+          scrollHeight: contentScrollHeight,
+          offsetHeight: contentOffsetHeight,
+        } = contentRef.current;
+        const deltaY = (evt.clientY - scrollStartPosition) * (contentOffsetHeight / thumbHeight);
+        contentRef.current.scrollTop = Math.min(
+          initialScrollTop + deltaY,
+          contentScrollHeight - contentOffsetHeight
+        );
+      }
+    },
+    [isDragging, scrollStartPosition, thumbHeight, initialScrollTop]
+  );
+
+  useEffect(() => {
+    if (contentRef.current && scrollTrackRef.current) {
+      const ref = contentRef.current;
+      const { clientHeight: trackSize } = scrollTrackRef.current;
+      observer.current = new ResizeObserver(() => {
+        handleResize(ref, trackSize);
+      });
+      observer.current?.observe(ref);
+      ref.addEventListener('scroll', handleThumbPosition);
+      return () => {
+        observer.current?.unobserve(ref);
+        ref.removeEventListener('scroll', handleThumbPosition);
+      };
+    }
+    return () => {
+    };
+  });
+
+  return (
+    <div className="scroll-bar-area" onWheel={handleWheel}>
+      <div className="scroll-bar-area__content" ref={contentRef}>
+        {children}
+      </div>
+      <div className="scroll-bar-area__scrollbar">
+        <div
+          className="scroll-bar-area__scrollbar-track"
+          ref={scrollTrackRef}
+          onClick={handleTrackClick}
+          role="none"
+        />
+        <div
+          className="scroll-bar-area__scrollbar-thumb"
+          onPointerDown={handleThumbPointerDown}
+          onPointerUp={handleThumbPointerUp}
+          onPointerLeave={handleThumbPointerUp}
+          onPointerMove={handleThumbPointerMove}
+          ref={scrollThumbRef}
+          style={{
+            height: thumbHeight,
+            cursor: isDragging ? 'grabbing' : 'grab'
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+ScrollBarArea.defaultProps = {
+  children: PropTypes.node,
+};
+
+export default ScrollBarArea;
